f2e59e1e567d2fc7acb74db1f4e67afa3ce83b1f
Lezt Waper <leztwaper@gmail.com>
2021-11-01 10:37:28 +0700

    change something

--- study/Program.cs
+++ study/Program.cs
@@ -8,22 +8,11 @@ namespace study
     {
         static void Main(string[] args)
         {
-            var fibonacciNumbers = new List<int>{ 1, 1};
-             
-            int position = 20 ;
-
-            while (fibonacciNumbers.Count <= position )
-            {
-                int temp1 = fibonacciNumbers[fibonacciNumbers.Count - 1];
-                int temp2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
-                fibonacciNumbers.Add(temp1 + temp2);
-            }
-
-            foreach (int i in fibonacciNumbers)
-            {
-                Console.Write($"{i} \n");
-            }
-            
+            char n = 'u';
+            char i = n;
+            Console.WriteLine($"{n}{i}");
+            n = 'h';
+            Console.WriteLine($"{n}{i}");
         }
         
 
